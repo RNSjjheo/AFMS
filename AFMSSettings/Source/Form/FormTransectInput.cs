4445712973d9ab053a700a5e75d1fba1720228d0
RNSjjheo <jjheo@rnsea.com>
2026-08-24 19:14:32 +0900

    json 포멧 수정

--- AFMSSettings/Source/Form/FormTransectInput.cs
+++ AFMSSettings/Source/Form/FormTransectInput.cs
@@ -221,7 +221,7 @@ namespace AFMSSettings
             DateTime now = DateTime.Now;
             List<object> transects = new List<object>(distances.Count);
             for (int i = 0; i < distances.Count; i++) transects.Add(new { no = i + 1, distance = distances[i] });
-            string json = JsonSerializer.Serialize(transects);
+            string json = JsonSerializer.Serialize(new { transects });
 
             QueryBuilderInsert query = new QueryBuilderInsert();
             query.Table = FbtAFMSHydroTransect.TABLE_NAME;
