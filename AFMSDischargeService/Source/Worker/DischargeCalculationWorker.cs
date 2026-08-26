6a6e58a9ce0eb91af5e3848a9bc48214f38ebfc1
RNSjjheo <jjheo@rnsea.com>
2026-08-26 17:39:42 +0900

    a

---
+++ AFMSDischargeService/Source/Worker/DischargeCalculationWorker.cs
@@ -0,0 +1,29 @@
+﻿using AFMSDll;
+using log4net.Repository.Hierarchy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFMSDischargeService
+{
+    internal class DischargeCalculationWorker : BackgroundService
+    {
+        private static readonly TimeSpan PollInterval = TimeSpan.FromSeconds(10);
+        private List<_QBase> calculators = new();
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using PeriodicTimer timer = new(PollInterval);
+
+            try
+            {
+                while (await timer.WaitForNextTickAsync(stoppingToken))
+                {
+
+                }
+            }
+            catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
+            {
+            }
+        }
+    }
+}
