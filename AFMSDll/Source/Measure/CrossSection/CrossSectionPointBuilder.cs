97fcdff8e21889849c7456c423d004cb611b4550
RNSjjheo <jjheo@rnsea.com>
2026-08-26 09:02:45 +0900

    a

---
+++ AFMSDll/Source/Measure/CrossSection/CrossSectionPointBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFMSDll
+{
+    public static class CrossSectionPointBuilder
+    {
+        public static CrossSectionPointCollection Build(IEnumerable<CrossSectionPoint> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            CrossSectionPointCollection result = new();
+            result.AddRange(source);
+            return result;
+        }
+
+        public static CrossSectionPointCollection Build<T>(IEnumerable<T> source, Func<T, CrossSectionPoint> converter)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(converter);
+
+            CrossSectionPointCollection result = new();
+
+            foreach (T item in source)
+            {
+                CrossSectionPoint point = converter(item) ?? throw new InvalidOperationException("CrossSectionPoint 변환 결과가 null입니다.");
+                result.Add(point);
+            }
+
+            return result;
+        }
+    }
+}
