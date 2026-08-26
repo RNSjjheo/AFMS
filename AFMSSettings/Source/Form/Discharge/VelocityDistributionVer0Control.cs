1a50bdee26bfd4687a6113e1705e0feecfb67ee2
RNSjjheo <jjheo@rnsea.com>
2026-08-26 15:40:33 +0900

    a

--- AFMSSettings/Source/Form/Discharge/VelocityDistributionVer0Control.cs
+++ AFMSSettings/Source/Form/Discharge/VelocityDistributionVer0Control.cs
@@ -42,8 +42,8 @@ namespace AFMSSettings
             main.Margin = Padding.Empty;
             main.ColumnCount = 2;
             main.RowCount = 1;
-            main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60F));
-            main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 70F));
+            main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
             main.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
 
             AFMSSectionPanel settings = CreateSectionPanel();
