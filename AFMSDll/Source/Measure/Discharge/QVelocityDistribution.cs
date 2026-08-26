0ba664aaecd6acaa16ee08a71971bdbe8502958f
RNSjjheo <jjheo@rnsea.com>
2026-08-26 15:20:46 +0900

    a

--- AFMSDll/Source/Measure/Discharge/QVelocityDistribution.cs
+++ AFMSDll/Source/Measure/Discharge/QVelocityDistribution.cs
@@ -6,5 +6,17 @@ namespace AFMSDll
             : base(DischargeMethod.VeloDist)
         {
         }
+
+        public static AFMSMathLabel GetExample()
+        {
+            AFMSMathLabel item = new AFMSMathLabel();
+            item.ClearMath();
+            item.AddText("Q = ");
+            item.Add('∫');
+            item.Add("A", AFMSMathTextType.Subscript);
+            item.AddText(" u(x,y)dA");
+
+            return item;
+        }
     }
 }
