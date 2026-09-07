924d7c899c3708dfd46924e798f9bb53804b4b32
RNSjjheo <jjheo@rnsea.com>
2026-09-07 11:22:15 +0900

    a

---
+++ AFMSLoggerMonitors/Source/Main/FormMain.cs
@@ -0,0 +1,15 @@
+namespace AFMSLoggerMonitors
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
