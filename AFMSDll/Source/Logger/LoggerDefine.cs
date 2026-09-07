8165d0d3b4facdef7ced0d92e2cd76e4d0642af9
RNSjjheo <jjheo@rnsea.com>
2026-09-07 11:38:42 +0900

    a

---
+++ AFMSDll/Source/Logger/LoggerDefine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFMSDll
+{
+    public enum LoggerTcpPort
+    { 
+        ChannelMaster = 12350,
+        EWSVConvertor = 12355,
+        VideoHydrosem = 12365,
+    }
+    internal class LoggerDefine
+    {
+    }
+}
--- AFMSLoggerMonitors/Source/Main/FormMain.cs
+++ AFMSLoggerMonitors/Source/Main/FormMain.cs
@@ -1,10 +1,26 @@
+using AFMSDll;
+
 namespace AFMSLoggerMonitors
 {
-    public partial class FormMain : Form
+    public partial class FormMain : AFMSForm
     {
+        private AFMSTabBar uiTabControl;
+        private TableLayoutPanel uiTpMain;
         public FormMain()
         {
             InitializeComponent();
+            this.Text = "Total Logger Monitoring";
+            ClientSize = new Size(800, 450);
+
+            uiTpMain = new TableLayoutPanel();
+            uiTpMain.Dock = DockStyle.Fill;
+            uiTpMain.RowStyles.Clear();
+            uiTpMain.ColumnStyles.Clear();
+            uiTpMain.RowCount = 2;
+            uiTpMain.ColumnCount = 1;
+            uiTpMain.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            uiTpMain.RowStyles.Add(new RowStyle(SizeType.Percent,100F));
+            uiTpMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
         }
 
         private void Form1_Load(object sender, EventArgs e)
